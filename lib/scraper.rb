a278dd11b9b5b41eccc26a3e73bf3577bc499d42
Niusha H <niusha.hadziomerovic@gmail.com>
2019-06-03 17:44:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,6 +10,17 @@ students = []
     #array of hashes that represents a single student [hash, hash, hash]
     index_url = http://178.128.13.85:50977/fixtures/student-site/
     
+    index_page = Nokogiri::HTML(open(index_url))
+    student_array = []
+    index_page.css(".roster-cards-container").each do |student|
+      student.css(".student-card a").each do |info|
+        student_name = info.css(".student-name").text
+        student_location = info.css(".student-location").text
+        student_profile = "#{info.attr('href')}"
+        student_array << {:name => student_name, :location => student_location, :profile_url => student_profile}
+      end
+    end
+    student_array
     
   end
 
