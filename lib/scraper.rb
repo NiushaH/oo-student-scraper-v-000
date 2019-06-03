ccdf9920a380988de8e4b61501505d8136d31745
Niusha H <niusha.hadziomerovic@gmail.com>
2019-06-03 18:06:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ students = []
   def self.scrape_profile_page(profile_url)
     #use Nokogiri and Open-URI to access page and RETURN hash that describes student
 
-    profile_url = :profile_url
+    # profile_url = :profile_url
     
     scraped_student = Nokogiri::HTML(open(profile_url))
     attributes_hash = {}
