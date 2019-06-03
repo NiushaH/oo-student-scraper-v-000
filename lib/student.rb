1848236010fa7e0870103f5af8a5b9a1cf7f6dc7
Niusha H <niusha.hadziomerovic@gmail.com>
2019-06-03 17:59:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @student = student_hash.name
-    
+    student_hash.each {|k, v| self.send(("#{k}="), v)}
     @@all << self
   end 
 
@@ -14,15 +13,16 @@ class Student
     #the argument here is the students array from the Scraper class
     #this method should iterate over the array of hashes and create a new individual student using each hash using initialize 
     
+    students_array.each {|student| student = self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
     #attributes hash from scape_profile_page method
-    
+
+    attributes_hash.each {|k, v| self.send(("#{k}="), v)}
   end
 
   def self.all
     @@all
   end
 end
-
