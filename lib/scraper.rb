e0d8522ea2dae91f099dd7a4e1faefcb4887bcf6
Niusha H <niusha.hadziomerovic@gmail.com>
2019-06-03 17:51:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,16 +8,18 @@ students = []
 
   def self.scrape_index_page(index_url)
     #array of hashes that represents a single student [hash, hash, hash]
-    index_page = Nokogiri::HTML(open(index_url))
-    student_array = []
     
-    index_page.css(".roster-cards-container").each do |student|
-      student.css(".student-card a").each do |info|
-        student_name = info.css(".student-name").text
-        student_location = info.css(".student-location").text
-        student_profile = "#{info.attr('href')}"
-        student_array << {:name => student_name, :location => student_location, :profile_url => student_profile}
-      end
+    # html = open(index_url)
+    # index = Nokogiri::HTML(html)
+    index_page = Nokogiri::HTML(open(index_url))
+
+    student_array = []
+    index.css(".student-card a").each do |student| 
+      student_hash = {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.attribute("href").value}
+        student_array << student_hash
     end
     student_array
   end
